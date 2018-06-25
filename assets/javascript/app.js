548f931a859fc758b094dd2e394b373570e3ea10
Joseph V <joseph.vonedwins@yahoo.com>
2018-06-25 18:40:24 -0400

    add ability to add and remove favorties

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,8 +11,8 @@ var APIkey = 'tpOBhMb9bMKdBzmRRjzV11nmEBK126qW';
 $(document).ready(function () {
     createButtons();
 
-//Clicked functions section
-//========================
+    //Clicked functions section
+    //========================
 
     //Function to add a button
     $("#new-category-button").on('click', function () {
@@ -27,11 +27,11 @@ $(document).ready(function () {
     });
 
     //Function to add GIFs to gif-box
-    $(document).on('click', '.topicButton', function() {
+    $(document).on('click', '.topicButton', function () {
         var giphTopic = $(this).val();
         giphTopic = giphTopic.replace(" ", "+");
         var queryURL = 'http://api.giphy.com/v1/gifs/search?q=' + giphTopic + '&api_key=tpOBhMb9bMKdBzmRRjzV11nmEBK126qW&limit=10'
-        console.log(queryURL);
+        
 
         //AJAX call to GIPHY API
         $.ajax({
@@ -42,7 +42,7 @@ $(document).ready(function () {
 
 
     //Function to Start/Stop GIFs
-    $(document).on('click', '.gif', function() {
+    $(document).on('click', '.gif', function () {
 
         if ($(this).attr('data-state') == 'still') {
             $(this).attr('src', $(this).attr('data-animate'));
@@ -53,9 +53,43 @@ $(document).ready(function () {
         };
     });
 
+    //Function to add gif to favorites
+    $(document).on('click', '.favorite-button', function () {
+        //create new image in favorites
+        var newGIPH = $("<img class='mb-1 favorite-gif'>");
+        var newSrc = $(this).attr('data-still');
+        newGIPH.attr('src', newSrc);
+        var newAnimate = $(this).attr('data-animate');
+        newGIPH.attr('data-animate', newAnimate);
+        newGIPH.attr('data-still', newSrc);
+        newGIPH.attr('data-state', 'still');
+        newGIPH.addClass('gif');
+        
 
-//Called functions section
-//========================
+
+        //create remove button in favorites
+        var newButton = $("<button>");
+        newButton.addClass('btn btn-sm btn-dark remove-button');
+        newButton.text('Remove Favorite');
+
+
+
+        //create new div, append contents, and append it to the favorites section
+        var favoritesDiv = $("<div>");
+        favoritesDiv.append(newGIPH);
+        favoritesDiv.append(newButton);
+        $('#favorites-box').append(favoritesDiv);
+    });
+
+
+    //Function to remove favorites when clicked
+        $(document).on('click', '.remove-button', function() {
+            $(this).closest('div').remove('');
+        });
+
+
+    //Called functions section
+    //========================
 
     //Function to create buttons
     function createButtons() {
@@ -69,8 +103,10 @@ $(document).ready(function () {
         }
     };
 
+
+    //Function to update the gif section of the page when the item is clicked
     function updatePage(response) {
-        console.log(response);
+
         //change results to array to minimize typing
         var dataArray = response.data;
 
@@ -78,32 +114,33 @@ $(document).ready(function () {
 
         for (i = 0; i < dataArray.length; i++) {
 
-        //Create Image for new GIF
-        var newGIPH = $('<img class="mb-1">');
-        newGIPH.attr('src', dataArray[i].images.fixed_height_still.url);
-        console.log(dataArray[i].images.fixed_height_still.url);
-        newGIPH.attr('data-animate', dataArray[i].images.fixed_height.url);
-        newGIPH.attr('data-still', dataArray[i].images.fixed_height_still.url);
-        newGIPH.attr('data-state', 'still');
-        newGIPH.addClass('gif');
+            //Create Image for new GIF
+            var newGIPH = $('<img class="mb-1">');
+            newGIPH.attr('src', dataArray[i].images.fixed_height_still.url);
+            newGIPH.attr('data-animate', dataArray[i].images.fixed_height.url);
+            newGIPH.attr('data-still', dataArray[i].images.fixed_height_still.url);
+            newGIPH.attr('data-state', 'still');
+            newGIPH.addClass('gif');
 
-        //Create a paragraph to house the GIF rating
-        newGIPHrating = $('<p class="gif-rating">');
-        newGIPHrating.text('Rating: ' + dataArray[i].rating);
+            //Create a paragraph to house the GIF rating
+            newGIPHrating = $('<p class="gif-rating">');
+            newGIPHrating.text('Rating: ' + dataArray[i].rating);
 
-        //Create a favorite button
-        var favoriteButton = $('<button class="favorite-button btn-sm btn-outline-dark">');
-        favoriteButton.text('Favorite');
+            //Create a favorite button
+            var favoriteButton = $('<button class="favorite-button btn-sm btn-outline-dark">');
+            favoriteButton.attr('data-animate', dataArray[i].images.fixed_height.url);
+            favoriteButton.attr('data-still', dataArray[i].images.fixed_height_still.url);
+            favoriteButton.text('Add to Favorites');
 
 
-        //Create box to house the new GIF and rating
-        var newGIPHbox = $('<div class="div-GIPH-box">')
-        newGIPHbox.append(newGIPH);
-        newGIPHbox.append(newGIPHrating);
-        newGIPHbox.append(favoriteButton);
-       $('#giph-box').prepend(newGIPHbox);
-    };
-}; //Close loop that creates the correct number of GIFs from dataArray 
+            //Create box to house the new GIF and rating
+            var newGIPHbox = $('<div class="div-GIPH-box">')
+            newGIPHbox.append(newGIPH);
+            newGIPHbox.append(newGIPHrating);
+            newGIPHbox.append(favoriteButton);
+            $('#giph-box').prepend(newGIPHbox);
+        };
+    }; //Close loop that creates the correct number of GIFs from dataArray 
 
 
 });//Close document.ready function
